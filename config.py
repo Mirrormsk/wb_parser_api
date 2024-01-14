b22aa941996a700a7407209cbf863ae0c1a1873a
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-14 16:42:36 +0300

    added database params

--- config.py
+++ config.py
@@ -4,6 +4,12 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     API_ID: int
     API_HASH: str
+    BOT_TOKEN: str
+    DB_URL: str
+    DB_CREATE_URL: str
 
     class Config:
         env_file = ".env"
+
+
+settings = Settings()
