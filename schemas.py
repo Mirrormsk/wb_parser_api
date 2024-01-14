15cad3100dc936a2fdd9ec924094b385f342c1fa
Maxim Donchuk <m.donchuk@gmail.com>
2024-01-14 16:43:32 +0300

    added schema for message sending

--- schemas.py
+++ schemas.py
@@ -4,3 +4,11 @@ from pydantic import BaseModel
 class PhoneSchema(BaseModel):
     """Schema for Phone"""
     phone: int
+
+
+class SendMessageSchema(BaseModel):
+    """Schema for POST /messages"""
+
+    message_text: str
+    from_phone: str
+    username: str
